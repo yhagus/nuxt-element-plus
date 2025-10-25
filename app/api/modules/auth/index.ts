bf2a779362d912d6cd81e4a0302292cc441033d4
Bagus Pranowo <bagus.pranowo@students.amikom.ac.id>
2025-10-25 23:24:16 +0700

    Refactor authentication functions in index.ts

--- app/api/modules/auth/index.ts
+++ app/api/modules/auth/index.ts
@@ -1,21 +1,53 @@
 import type { NitroFetchOptions } from 'nitropack';
 
-export function getAccessToken(form: UseForm, options?: NitroFetchOptions<any>) {
-  return form.post('/v1/auth/login', options);
+interface LoginResponse {
+  token_type: 'Bearer';
+  expires_in: number;
+  access_token: string;
+  refresh_token: string;
+}
+
+export function authorize(token?: string | null) {
+  const config = useRuntimeConfig();
+  return $fetch<User>('/v1/auth/authorize', {
+    baseURL: config.public.apiUrl,
+    headers: {
+      accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  });
+}
+export function login(options?: NitroFetchOptions<any>) {
+  return (useNuxtApp().$api as typeof $fetch)<LoginResponse>('/v1/auth/login', options);
+}
+export function logout(options?: NitroFetchOptions<any>) {
+  const config = useRuntimeConfig();
+  const accessToken = useCookie(ACCESS_TOKEN);
+  return $fetch('/v1/auth/logout', {
+    baseURL: config.public.apiUrl,
+    method: 'POST',
+    headers: {
+      authorization: `Bearer ${accessToken.value}`,
+    },
+    ...options,
+  });
 }
 export function refresh(options?: NitroFetchOptions<any>) {
-  const { $api } = useNuxtApp();
-  return $api('/v1/auth/refresh', options);
-}
-export function getProfile(token?: string) {
-  const { $api } = useNuxtApp();
-  const options: any = {};
-
-  if (token) {
-    options.headers = {
-      authorization: `Bearer ${token}`,
-    };
-  }
-
-  return $api<User>('/v1/auth/me', options);
+  const { $config } = useNuxtApp();
+  const refreshToken = useCookie<string>(REFRESH_TOKEN);
+  return $fetch<LoginResponse>('/v1/auth/refresh', {
+    baseURL: $config.public.apiUrl,
+    method: 'POST',
+    headers: {
+      authorization: `Bearer ${refreshToken.value}`,
+      accept: 'application/json',
+    },
+    onResponse({response}) {
+      if (response.ok) {
+        refreshCookie(ACCESS_TOKEN);
+        refreshCookie(REFRESH_TOKEN);
+      }
+    },
+    ...options,
+  });
 }
