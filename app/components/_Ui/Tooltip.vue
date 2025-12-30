7e902f550b767b591049756cfd5ee14ad287d555
bagus <bagus.pranowo@vnt.co.id>
2025-12-30 22:02:57 +0700

    feat: add tooltip base component

---
+++ app/components/_Ui/Tooltip.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface TooltipProps {
+  content?: string;
+  placement?: string;
+  effect?: 'dark' | 'light' | 'custom';
+  showAfter?: number;
+  hideAfter?: number;
+  disabled?: boolean;
+  teleported?: boolean;
+}
+
+const props = defineProps<TooltipProps>();
+</script>
+
+<template>
+  <el-tooltip
+    :content="props.content"
+    :placement="props.placement"
+    :effect="props.effect"
+    :show-after="props.showAfter"
+    :hide-after="props.hideAfter"
+    :disabled="props.disabled"
+    :teleported="props.teleported"
+  >
+    <slot />
+    <template v-if="$slots.content" #content>
+      <slot name="content" />
+    </template>
+  </el-tooltip>
+</template>
+
+<style scoped lang="scss">
+
+</style>
