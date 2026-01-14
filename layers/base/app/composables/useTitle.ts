ab17450e2831008b53dbca7daf98778db1e0dd51
bagus <bagus.pranowo@vnt.co.id>
2026-01-14 11:36:30 +0700

    chore: types fixed

--- layers/base/app/composables/useTitle.ts
+++ layers/base/app/composables/useTitle.ts
@@ -1,16 +1,16 @@
 export function useTitle() {
   const nuxtApp = useNuxtApp();
   const title = ref<string>(nuxtApp.$config.public.appTitle ?? '');
-  const getTitle = () => {
+  const getTitle = (): string => {
     return title.value;
   };
-  const setTitle = (value: string) => {
+  const setTitle = (value: string): void => {
     title.value = value;
     useHead({
       title: title.value,
     });
   };
-  const resetTitle = () => {
+  const resetTitle = (): void => {
     title.value = nuxtApp.$config.public.appTitle;
     useHead({
       title: title.value,
